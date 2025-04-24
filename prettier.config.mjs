0719ad47d26478b0b7607a6b3e314b8d0d6cc32a
Amund Lindstøl <alindstol@gmail.com>
2025-04-24 13:57:36 +0200

    prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,22 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	trailingComma: 'es5',
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	useTabs: true,
+	overrides: [
+		{
+			files: '*.json',
+			options: {
+				tabWidth: 2,
+				useTabs: false,
+			},
+		},
+	],
+}
+
+export default config
