cf54800c4726b7f4189200bf005f81335320fa74
Amund Lindstøl <alindstol@gmail.com>
2025-04-29 14:37:03 +0200

    ignore eslint on build

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [new URL(`https://cdn.sanity.io/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/**`)],
 	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if the project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 }
 
 export default nextConfig
